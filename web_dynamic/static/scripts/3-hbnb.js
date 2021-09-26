433e78d7d4d851784d290db2662f18474191b64d
DreMukare <dremukare@gmail.com>
2021-09-26 11:27:43 +0000

    properly consuming api

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,7 +1,25 @@
 $(() => {
   $.post( "http://0.0.0.0:5001/api/v1/places_search/", {}, (data)=> {
   console.log(data);
-    data.forEach((item)=> {$('section.places').html(`<article>${item.name}</article>`)})
+    data.forEach((place)=> {$('section.places').html(`
+        <article>
+            <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+	    <div class="max_guest">${place.max_guest} ${place.max_guest !== 1 ? "Guests" : "Guest"}</div>
+            <div class="number_rooms">${place.number_rooms} ${place.number_rooms !== 1 ? "Bedrooms" : "Bedroom"}</div>
+            <div class="number_bathrooms">${place.number_bathroom} ${place.number_bathrooms !== 1 ? "Bathrooms" : "Bathroom"}</div>
+	  </div>
+	  <div class="user">
+            <b>Owner:</b> ${place.user.first_name } ${place.user.last_name}
+          </div>
+          <div class="description">
+	    ${place.description || safe }
+          </div>
+        </article>
+    `)})
     });
   $.get('http://0.0.0.0:5001/api/v1/status/', (res) => {
   console.log(res);
